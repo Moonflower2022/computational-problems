057a36f91cf9563aebb432d3d001d2530502bb01
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2024-05-07 17:50:51 -0700

    added functionality for multiple opponents

--- bidding_sim/evaluation.py
+++ bidding_sim/evaluation.py
@@ -2,23 +2,25 @@ import random
 import math
 import matplotlib.pyplot as plt
 
-gamma = 0.1
+l = 0.01
+n = 3
+
 bids = list(range(1, 101))
 value = 100
 
 def distribution(x):
-    return -math.log(x)/gamma # this is my way of sampling from my distribution
+    return -math.log(x)/l # this is my way of sampling from my distribution
 
-simulations = 10000
+simulations = 100000
 
-profit_averages = [sum((bid > distribution(random.random())) * (value - bid) for _ in range(simulations))/simulations for bid in bids]
+profit_averages = [sum(all((bid > distribution(random.random())) for _ in range(n)) * (value - bid) for _ in range(simulations))/simulations for bid in bids]
 
 sorted_bids = [bid for _, bid in sorted(zip(profit_averages, bids), reverse=True)]
 
 print("Ranked Bids:", sorted_bids)
 
 plt.plot(profit_averages)
-plt.title(f"Bidding Simulation for gamma = {gamma}")
+plt.title(f"Bidding Simulation for lambda = {l}, n = {n}")
 plt.xlabel("Bid Amount")
 plt.ylabel("Average Profit")
 plt.show()
