b91f3bb665c645a9c9f8e4a35387704ac76ed65d
Moonflower2022 <harrisonq125@gmail.com>
2025-03-21 19:14:43 -0700

    uncommented some code

--- probability_simulations/estimator_repeated_random_event.py
+++ probability_simulations/estimator_repeated_random_event.py
@@ -12,11 +12,11 @@ def get_num_events_for_repeat(n):
 if __name__ == "__main__":
     n = 263
 
-    # iterations = 1000000
-    # total_num_events_for_repeat = 0
-    # for _ in range(iterations):
-    #     total_num_events_for_repeat += get_num_events_for_repeat(n)
+    iterations = 1000000
+    total_num_events_for_repeat = 0
+    for _ in range(iterations):
+        total_num_events_for_repeat += get_num_events_for_repeat(n)
 
-    # print(total_num_events_for_repeat / iterations)
+    print(total_num_events_for_repeat / iterations)
     print(sum([math.factorial(n) * (k - 1) / (n ** k * math.factorial(n - k + 1)) for k in range(1, n + 1)]))
     print(sum([k * math.factorial(n) * (k - 1) / (n ** k * math.factorial(n - k + 1)) for k in range(1, n + 1)]))
