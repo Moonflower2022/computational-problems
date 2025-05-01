44f8167371da8fe1b897f1552e249e7a71ab8733
Moonflower2022 <harrisonq125@gmail.com>
2025-05-01 14:36:09 -0700

    added new idea for an agent based simulation
    added aging_rate to the differential sim

---
+++ coevolution/agent_sim.py
@@ -0,0 +1,7 @@
+# idea: 
+# two (or n) virus strains
+# population of people
+# the viruses have different genotypes, and can mutate when reproducing
+# the humans have immune systems of varying strength
+# after some time they develop cells to counter the virus
+# after a really long time they develop vaccines against specific strains of viruses
--- coevolution/differential_equations.py
+++ coevolution/differential_equations.py
@@ -15,6 +15,7 @@ class Simulation:
             modulation_factor=0.1, # α (alpha)
             modulation_rate=1.0, # r
             infected_population_carrying_capacity=1.0, # K
+            aging_rate=0.05
             ):
         self.death_rate = death_rate
         self.birth_rate = birth_rate
@@ -25,25 +26,35 @@ class Simulation:
         self.modulation_factor = modulation_factor
         self.modulation_rate = modulation_rate
         self.infected_population_carrying_capacity = infected_population_carrying_capacity
+
+        # ADDED VARIABLES
+
+        self.aging_rate = aging_rate
+
+        # running variables
+        self.transmission_rate = self.base_transmission_rate
         self.susceptible_population1 = 0.475
         self.susceptible_population2 = 0.475
         self.infected_population1 = 0.01
         self.infected_population2 = 0.05
         self.recovered_population = 0.0
 
-    def get_populations(self):
-        return [self.susceptible_population1, self.susceptible_population2, self.infected_population1, self.infected_population2, self.recovered_population]
+        # static variables
+        self.num_data_points = 6
+
+    def get_data(self):
+        return [self.susceptible_population1, self.susceptible_population2, self.infected_population1, self.infected_population2, self.recovered_population, self.transmission_rate]
 
 
     def step(self, delta_time):
         total_infected = self.infected_population1 + self.infected_population2
-        transmission_rate = self.base_transmission_rate * (1 + self.modulation_factor * self.modulation_rate * total_infected * (1 - total_infected / self.infected_population_carrying_capacity))
+        self.transmission_rate = self.base_transmission_rate * (1 + self.modulation_factor * self.modulation_rate * total_infected * (1 - total_infected / self.infected_population_carrying_capacity))
 
-        susceptible_population1_change = self.birth_rate - transmission_rate * self.susceptible_population1 * (self.infected_population1 + self.infected_population2) + self.reinfection_rate * self.recovered_population - self.death_rate * self.susceptible_population1
-        susceptible_population2_change = -transmission_rate * self.susceptible_population2 * (self.infected_population1 + self.infected_population2) - self.death_rate * self.susceptible_population2
+        susceptible_population1_change = self.birth_rate -self.aging_rate * self.susceptible_population1 - self.transmission_rate * self.susceptible_population1 * (self.infected_population1 + self.infected_population2) + self.reinfection_rate * self.recovered_population - self.death_rate * self.susceptible_population1
+        susceptible_population2_change = self.aging_rate * self.susceptible_population1 - self.transmission_rate * self.susceptible_population2 * (self.infected_population1 + self.infected_population2) - self.death_rate * self.susceptible_population2
 
-        infected_population1_change = transmission_rate * self.susceptible_population2 * self.infected_population1 - self.recovery_rate * self.infected_population1 - self.waning_immunity * self.infected_population1 - self.death_rate * self.infected_population1
-        infected_population2_change = transmission_rate * self.susceptible_population1 * self.infected_population2 - self.recovery_rate * self.infected_population2 - self.waning_immunity * self.infected_population2 - self.death_rate * self.infected_population2
+        infected_population1_change = self.transmission_rate * self.susceptible_population2 * self.infected_population1 - self.recovery_rate * self.infected_population1 - self.waning_immunity * self.infected_population1 - self.death_rate * self.infected_population1
+        infected_population2_change = self.transmission_rate * self.susceptible_population1 * self.infected_population2 - self.recovery_rate * self.infected_population2 - self.waning_immunity * self.infected_population2 - self.death_rate * self.infected_population2
 
         recovered_population_change = self.recovery_rate * (self.infected_population1 + self.infected_population2) - self.reinfection_rate * self.recovered_population - self.death_rate * self.recovered_population
 
@@ -57,20 +68,21 @@ class Simulation:
 
 def main():
     simulation = Simulation()
+    num_datapoints = simulation.num_data_points
     iterations = 100
     total_time = 100
     delta_time = total_time / iterations
 
-    populations_list = np.zeros((iterations, 5))
+    populations_list = np.zeros((iterations, num_datapoints))
 
     for i in range(iterations): 
-        populations_list[i] = np.array(simulation.get_populations())
+        populations_list[i] = np.array(simulation.get_data())
         simulation.step(delta_time)
 
     plt.figure(figsize=(10, 6))
     x = np.linspace(0, total_time, iterations)
-    labels = ["susceptible1", "susceptible2", "infected1", "infected2", "recovered"]
-    for i in range(5):
+    labels = ["susceptible1", "susceptible2", "infected1", "infected2", "recovered", "transmission_rate"]
+    for i in range(num_datapoints):
         plt.plot(x, populations_list[:, i], label=labels[i])
 
     plt.legend()
