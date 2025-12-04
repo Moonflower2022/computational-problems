9a9ee6a6c3cfc371996ef72cafd3f467991404de
Moonflower2022 <harrisonq125@gmail.com>
2025-12-04 15:47:58 -0800

    beginning of 2.1 aoc 2025

---
+++ advent_of_code_2025/2-Gift-Shop/part_1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+long long get_number_length(long long number) {
+    long long length = 0;
+    while (number) {
+        number /= 10;
+        length++;
+    }
+    return length;
+}
+
+long long get_repetition_divisor(long long id_length, long long length) {
+    long long repetition_count = id_length / length;
+
+    long long divisor = 0;
+    long long power = 1;
+    for (long long i = 0; i < repetition_count; i++) {
+        divisor += power;
+        for (long long j = 0; j < length; j++) {
+            power *= 10;
+        }
+    }
+    return divisor;
+}
+
+bool is_fake(long long id) {
+    long long number_length = get_number_length(id);
+
+    for (long long length = 2; length <= number_length / 2; length++) {
+        if (
+            number_length % length == 0 &&
+            number_length / length > 1 &&
+            id % get_repetition_divisor(number_length, length) == 0
+        ) {
+            cout << "  found fake:" << id << endl;
+            return true;
+        }        
+    }
+    return false;
+}
+
+int main() {
+
+    ifstream puzzle_input("puzzle_input.txt");
+
+    string line;
+    getline(puzzle_input, line);
+
+    stringstream ss(line);
+
+    vector<pair<long long, long long>> id_ranges;
+
+    string pair_str;
+    while (getline(ss, pair_str, ',')) {
+        long long dash_location = pair_str.find('-');
+        long long start = stoll(pair_str.substr(0, dash_location));
+        long long end = stoll(pair_str.substr(dash_location + 1));
+        id_ranges.push_back({start, end});
+    }
+
+    long long fake_sum = 0;
+    for (const auto& range : id_ranges) {
+        for (long long id = range.first; id <= range.second; id++) {
+            if (is_fake(id)) {
+                fake_sum += id;
+            }
+        }
+    }
+    cout << fake_sum << endl;
+
+    return 0;
+}
